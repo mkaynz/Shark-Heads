847be229e00991bcac62bc6512682befc3d976eb
Cayla Taylor <ctylor50@memphis.edu>
2021-05-04 21:37:34 -0500

    Our history

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'home', to: 'pages#home', as: 'home'
 
+  get 'history', to: 'pages#history', as: 'history'
   get 'team', to: 'pages#team', as: 'team'
 
 end
